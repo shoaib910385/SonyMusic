dbbc153d18be861b44857b790769b8f7f779ae3e
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-25 13:57:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,10 +81,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/cgmtwh.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/taiwmx.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/cgmtwh.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/taiwmx.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/6tled7.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/cgmtwh.jpg"
