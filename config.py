58d7743b9b88cb1d34aeff34c6d8650f94b1f4fd
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-25 09:08:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shoaib910385/COLDAPK",
+    "https://github.com/shoaib910385/SonyMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
