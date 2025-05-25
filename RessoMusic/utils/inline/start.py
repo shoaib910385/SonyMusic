73b6ea505b2aa7e988a0735e273250d603bbbca7
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-25 15:43:18 +0530

    Update start.py

--- RessoMusic/utils/inline/start.py
+++ RessoMusic/utils/inline/start.py
@@ -26,7 +26,6 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-     InlineKeyboardButton(text=_["S_B_6"], callback_data="LG"),
             #InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
