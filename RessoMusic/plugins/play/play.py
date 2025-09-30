36e27b5a976900a646f90af72be5fcce737d533d
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-30 15:36:19 +0530

    Update play.py

--- RessoMusic/plugins/play/play.py
+++ RessoMusic/plugins/play/play.py
@@ -26,18 +26,8 @@ from config import BANNED_USERS, lyrical
 
 
 @app.on_message(
-    filters.command(
-        [
-            "play",
-            "vplay",
-            "cplay",
-            "cvplay",
-            "playforce",
-            "vplayforce",
-            "cplayforce",
-            "cvplayforce",
-        ]
-    )
+    filters.command(["play", "vplay", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce"], prefixes=["/", "!", "%", ",", "", ".", "@"])
+ 
     & filters.group
     & ~BANNED_USERS
 )
@@ -669,3 +659,4 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+
