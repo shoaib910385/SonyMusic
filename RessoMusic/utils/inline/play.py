d182c2b866835ed19942b6947c6f2e3bc30d26b9
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-30 13:49:58 +0530

    Update play.py

--- RessoMusic/utils/inline/play.py
+++ RessoMusic/utils/inline/play.py
@@ -52,6 +52,11 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [InlineKeyboardButton("˹ϻʏ ʜᴏϻє˼", url=f"http://t.me/itzdhruv1060"),
+         InlineKeyboardButton("˹ᴏᴡɴєʀ˼", url=f"http://t.me/itzdhruv"),
+
+        ],
+        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
